6382f26013ec60fa31e9030e5dba4fb959e81a4e
zwj <kuban@kubandeMac-mini-3.local>
2026-07-13 13:55:28 +0800

    demo

---
+++ example/example.go
@@ -0,0 +1,8 @@
+package example
+
+import "fmt"
+
+// 首字母大写，可以被包外访问
+func SayHello() {
+	fmt.Println("Hello")
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	var name string
+	var age int
+	name, age = "jack", 1
+}
